08cc26ef2d190a765b8dde28bdf6f49e59dd9635
mxghj <dest-supp@proton.me>
2026-03-15 11:16:48 +0200

    Remove unused parent tracking

--- src/tree.rs
+++ src/tree.rs
@@ -7,7 +7,6 @@ use crate::{NodeId, Style};
 #[derive(Clone, Debug)]
 pub struct LayoutTree {
     pub children: Vec<SmallVec<[NodeId; 6]>>,
-    pub parents: Vec<Option<NodeId>>,
     pub styles: Vec<Style>,
 }
 
@@ -15,7 +14,6 @@ impl LayoutTree {
     pub fn new() -> Self {
         Self {
             styles: Vec::new(),
-            parents: Vec::new(),
             children: Vec::new(),
         }
     }
@@ -28,7 +26,6 @@ impl LayoutTree {
         // Push to tree
         {
             self.styles.push(style);
-            self.parents.push(None);
             self.children.push(SmallVec::new());
         }
 
@@ -38,14 +35,10 @@ impl LayoutTree {
     /// Set the parent to nodes
     pub fn add_children(&mut self, parent: NodeId, children: &[NodeId]) {
         self.children[parent].extend_from_slice(children);
-        for &child in children {
-            self.parents[child] = Some(parent);
-        }
     }
 
     /// Set the parent to a node
     pub fn add_child(&mut self, parent: NodeId, child: NodeId) {
         self.children[parent].push(child);
-        self.parents[child] = Some(parent);
     }
 }
